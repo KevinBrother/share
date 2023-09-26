8e2082592b3c424cc5a200fc2c50159752651367
caojunjie <1301239018@qq.com>
2023-09-26 14:10:05 +0800

    feat(python) settrace

--- py/debug.py
+++ py/debug.py
@@ -1,10 +1,51 @@
 import sys
+def trace_dispatch( frame, event, arg):
+    """Dispatch a trace function for debugged frames based on the event.
 
+    This function is installed as the trace function for debugged
+    frames. Its return value is the new trace function, which is
+    usually itself. The default implementation decides how to
+    dispatch a frame, depending on the type of event (passed in as a
+    string) that is about to be executed.
 
-def debug_trace(frame, event, asd):
-    print(frame,event,asd)
+    The event can be one of the following:
+        line: A new line of code is going to be executed.
+        call: A function is about to be called or another code block
+                is entered.
+        return: A function or other code block is about to return.
+        exception: An exception has occurred.
+        c_call: A C function is about to be called.
+        c_return: A C function has returned.
+        c_exception: A C function has raised an exception.
 
-sys.settrace(debug_trace)
+    For the Python events, specialized functions (see the dispatch_*()
+    methods) are called.  For the C events, no action is taken.
+
+    The arg parameter depends on the previous event.
+    """
+    # if self.quitting:
+    #     return # None
+    if event == 'line':
+        input()
+        print('line')
+        print(frame, event, arg)
+    elif event == 'call':
+        print('call')
+    elif event == 'return':
+        print('return')
+    elif event == 'exception':
+        print('exception')
+    elif event == 'c_call':
+        print('c_call')
+    elif event == 'c_exception':
+        print('c_exception')
+    elif event == 'c_return':
+        print('c_return')
+    else:
+        print('bdb.Bdb.dispatch: unknown debugging event:', repr(event))
+    return trace_dispatch
+
+sys.settrace(trace_dispatch)
 
 exec(compile("""
 a = 1
