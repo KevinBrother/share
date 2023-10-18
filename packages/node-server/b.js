5a2f06bf98eb20254804134964b7f141e0886e55
caojunjie <1301239018@qq.com>
2023-10-18 16:36:49 +0800

    feat(node) 调试 node 项目

--- packages/node-server/b.js
+++ packages/node-server/b.js
@@ -4,6 +4,7 @@ const port = 3002;
 let b = 1;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  console.log('b.js', b);
   res.setHeader('Content-Type', 'text/plain');
   res.end(`Hello, b World!  ${b++} ==== ${port}`);
 });
--- packages/signalFile/index.js
+++ packages/signalFile/index.js
@@ -4,7 +4,7 @@ const fruits = ['apple', 'banana', 'orange'];
 function abc(c) {
   return 12 + c;
 }
-
+console.log('signalFile index.js fruits', fruits)
 const b = abc(a);
 
 console.log('signalFile index.js b', b)
