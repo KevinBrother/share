72646b29d1123feca2038c9d5fc168c1eb743650
caojunjie <1301239018@qq.com>
2024-03-01 17:39:07 +0800

    feat(project) webpack项目基础环境搭建

---
+++ mobile-adapt-share/src/app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Foo } from './pages'
+
+ReactDom.render(<Foo></Foo>, document.getElementById('root'));
+
+
---
+++ mobile-adapt-share/src/pages/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import img from '@assert/img.png';
+import '@assert/div.css';
+import '@assert/img.less';
+
+export function Foo() {
+
+  return (
+    <>
+      <img src={img} alt="" />
+    </>
+  );
+}
